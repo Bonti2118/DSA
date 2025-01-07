89b4ab52d818639a61df35c06454c0f6ce3179c4
bonti2118 <bontibaruah2118@gmail.com>
2025-01-07 22:16:00 +0530

    Patterns

---
+++ poly.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class poly {
+  public static void main(String args[]) {
+    int a=3, b=4, m=1;
+   
+    for(int i=1;i <=a;i++){     
+      for(int j= 1; j<=b;j++){                                  
+        System.out.print(m);
+        m++;
+      }
+      System.out.println();
+    }
+
+  }
+
+}
