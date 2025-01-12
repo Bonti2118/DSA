4c9242876e0017a5269cc9c26f0b343d3527c78d
bonti2118 <bontibaruah2118@gmail.com>
2025-01-12 21:44:34 +0530

    merged

--- Patterns/InvertedTriangle.java
+++ Patterns/InvertedTriangle.java
@@ -1,27 +1,12 @@
-package Patterns;
+class invertedTriangle{
+    public static void main(String[] args) {
+        int row = 5;
 
-public class InvertedTriangle {
-    public static void main(String args[]){
-        int row=5;
-        for(int i=1;i<=row;i++){
-            for(int j =5;j>=i;j--){
+        for(int i = 1; i <= row; i++){
+            for(int j= row; j>= i; j--){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    
 }
-// *****
-// ****
-// ***
-// **
-// *
-    
-
-    
-
-    
-
-    
-
