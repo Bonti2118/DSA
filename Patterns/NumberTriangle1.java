64a0a1743c0aad38680eac885e69212d89f1fd77
bonti2118 <bontibaruah2118@gmail.com>
2025-01-14 23:24:14 +0530

    NumberTriangle1

--- Patterns/NumberTriangle1.java
+++ Patterns/NumberTriangle1.java
@@ -2,10 +2,17 @@ public class NumberTriangle1 {
     public static void main(String args[]){
         int row=4;
         for(int i=1;i<=row;i++){
-            for(int j=1;j<=row;j++){
-                System.out.print("hi");
+            for(int j=1;j<=i;j++){
+             
+                System.out.print(i-j+1);
             }
             System.out.println();
         }
     }
 }
+
+// output:
+// 1
+// 21
+// 321
+// 4321
