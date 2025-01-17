169c34bf11418ca384bb615064f3dcc19c214d2f
bonti2118 <bontibaruah2118@gmail.com>
2025-01-18 00:41:23 +0530

    ABC pattern

---
+++ Patterns/ABCpatten.java
@@ -0,0 +1,21 @@
+public class ABCpatten {
+    
+    public static void main(String args[]){
+        int row = 3;
+        int col=3;
+       
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=col;j++){
+              char k = (char)('A'+j-1);  
+               System.out.print(k);           
+            } 
+            System.out.println();   
+        }
+    }
+}
+
+
+// output:
+// ABC
+// ABC
+// ABC
