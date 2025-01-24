31fa76af9784662577e54217b59ead16dc1b3e2c
bonti2118 <bontibaruah2118@gmail.com>
2025-01-25 00:37:32 +0530

    ABCTriangle2

--- Patterns/ABCTriangle2.java
+++ Patterns/ABCTriangle2.java
@@ -1,6 +1,6 @@
 public class ABCTriangle2 {
     public static void main(String[] args) {
-        int row =3;
+        int row =4;
         char k = 'A';
         for(int i=1;i<=row;i++){
             for(int j=1;j<=i;j++){
